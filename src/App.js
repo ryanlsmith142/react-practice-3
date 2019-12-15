1ca40ebc250addda28a647e657cf9f370b132deb
Ryan Smith <ryan.l.smith142@gmail.com>
2019-12-15 15:38:56 -0600

    worked with importing exporting react classes, as well as set up a seed file and played with using info from an outside source and display it using react components

--- src/App.js
+++ src/App.js
@@ -1,61 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-function Animal() {
-  return (
-      <div>
-        Roger
-      </div>
-  )
-}
+import animals from './seed.js';
 
 class AnimalList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            animals: [],
+        };
+    }
+
+    componentDidMount() {
+        this.setState({animals: animals});
+    }
+
   render() {
-    const animal = <Animal
+    const animalComponents = animals.map((animal) => (
+        <Animal
+            id={animal.id}
+            name={animal.name}
+            type={animal.type}
+        />
+        ));
 
-      name="Roger"
-
-
-
-    />;
     return (
         <div>
-          {animal}
+          {animalComponents}
         </div>
     );
 
   }
 }
 
-// class Animal extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         {this.name}
-//       </div>
-//     );
-//   }
-// }
+class Animal extends React.Component {
+  render() {
+    return (
+      <div>
+          {this.props.name}
+          {this.props.type}
+      </div>
+    );
+  }
+}
 
-export default Animal;
+export default AnimalList;
---
+++ src/seed.js
@@ -0,0 +1,16 @@
+
+    const animals = [
+        {
+            id: 1,
+            name: "Ava",
+            type: "Dog"
+        },
+        {
+            id: 2,
+            name: "Millie",
+            type: "Dog"
+        }
+    ];
+
+
+export default animals;
