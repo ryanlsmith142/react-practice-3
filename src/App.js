fc0c53a7df5350775e4d130522b49984235a5e24
Ryan Smith <ryan.l.smith142@gmail.com>
2019-12-16 09:25:31 -0600

    add semnatic ui framework, add generateLikeCount function and imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import animals from './seed.js';
+import generateLikeCount from './seed.js';
 
 class AnimalList extends React.Component {
     constructor(props) {
--- src/seed.js
+++ src/seed.js
@@ -1,16 +1,19 @@
+function generateLikeCount() {
+    return Math.floor((Math.random() * 50) + 15);
+}
 
-    const animals = [
-        {
-            id: 1,
-            name: "Ava",
-            type: "Dog"
-        },
-        {
-            id: 2,
-            name: "Millie",
-            type: "Dog"
-        }
-    ];
+
+
+const animals = [{
+    id: 1,
+    name: "Ava",
+    type: "Dog"
+}, {
+    id: 2,
+    name: "Millie",
+    type: "Dog"
+}
+];
 
 
 export default animals;
